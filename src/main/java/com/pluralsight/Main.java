77cdf22c1554a5237be7819fe96e090fcba1a8a4
Qi Qing <learn.qiqing@gmail.com>
2025-11-25 11:30:17 -0500

    fixed index problem from results column.

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -38,12 +38,12 @@ public class Main {
             ResultSet results = preparedStatement.executeQuery();
 
             while (results.next()) {
-                String productID = results.getString(0);
-                String productName = results.getString(1);
-                String unitPrice = results.getString(2);
-                String unitsInStock = results.getString(3);
+                String productID = results.getString(1);
+                String productName = results.getString(2);
+                String unitPrice = results.getString(3);
+                String unitsInStock = results.getString(4);
 
-                System.out.println("Product ID: " + productID + "Name: " + productName
+                System.out.println("Product ID: " + productID + " Name: " + productName
                 + "Price: " + unitPrice + "Stock: " + unitsInStock);
                 System.out.println("------------------------------");
 
@@ -52,7 +52,6 @@ public class Main {
             results.close();
             preparedStatement.close();
             connection.close();
-
 
         } catch (Exception e) {
             System.out.println("There's an error! ");
